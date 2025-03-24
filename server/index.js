f7897da8810ee7e582107f05949a27c3137cdcd1
muhumuzaa <muhumuzaamb@gmail.com>
2025-03-24 06:22:23 -0400

    made changes to server/package.json for azure deployment

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ import cors from 'cors'
 import connectToDb from './db/db.js'
 
 import authRouter from './routes/auth.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
+
+
 app.use(cors())
 
 app.use(express.json())
@@ -13,10 +20,18 @@ app.use(express.json())
 connectToDb().then(() =>{
     console.log('Db is connected!')
 
+    //serve static files
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
     app.use('/api/auth', authRouter)
 
-    app.listen(process.env.PORT, () =>{
-        console.log(`Server is running on port ${process.env.PORT}`);
+    //if no API routes match, send back index.html
+    app.get('*', (req, res) =>{
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
+    })
+    const PORT = process.env.PORT || 3001
+    app.listen( PORT, () =>{
+        console.log(`Server is running on port ${PORT}`);
         
     })
 }).catch(err => {
